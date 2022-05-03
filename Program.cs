1d87ccb5ec0f1736082d784eedebfe4104ab5583
Cristian Pîrvu <5724259+Kreestijan@users.noreply.github.com>
2022-05-03 21:17:53 +0300

    Added dictionary of 2499 five letter words

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿
-Console.WriteLine("Hello, World!");
+//string[] fiveLetterWords = File.ReadAllLines("C:\\_CodingVS\\WordleApp\\WordleApp\\dictionary.txt");
+
+string[] fiveLetterWords = File.ReadAllLines("dictionary.txt");
+
+foreach (string word in fiveLetterWords)
+{
+    Console.WriteLine(word);
+}
