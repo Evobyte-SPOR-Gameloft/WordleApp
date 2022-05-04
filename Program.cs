4fc7f1900537fd3ca93d5390747bce344779389c
Cristian Pîrvu <5724259+Kreestijan@users.noreply.github.com>
2022-05-04 11:15:32 +0300

    Did a bit of refactoring

--- Program.cs
+++ Program.cs
@@ -2,13 +2,6 @@
 //Reads a .txt file then adds each line as a string in an array
 string[] fiveLetterWords = File.ReadAllLines("dictionary.txt");
 
-//Picks an index at random from the string array then returns the string
-string GenerateRandomWord()
-            {
-                Random randomWordGenerator = new Random();
-                return fiveLetterWords[randomWordGenerator.Next(0, 2500)];
-            }
-
 bool quit = false;
 
 do
@@ -34,10 +27,11 @@ while (quit == false);
 
 void PlayTheGame()
 {
-    string chosenWord = GenerateRandomWord();
-    string guessedWord;
+    Random randomWordGenerator = new Random();
+    string chosenWord = fiveLetterWords[randomWordGenerator.Next(0, 2500)];
     Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     Console.WriteLine("Guess the five letter word!");
+    string guessedWord;
     guessedWord = Console.ReadLine();
     WordleApp.UtilityClass.LetterStateByIndex[] result = WordleApp.UtilityClass.LetterCheck(chosenWord, guessedWord);
 
